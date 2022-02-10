afaca730e953329ae30b2eaef45f33548fcc79c8
sriram bala <sr.bala.kkl@gmail.com>
2022-02-10 16:49:20 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,68 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'subscribeProj';
+
+  constructor() {
+    console.log('constructor is called \n \n \n \n \n ');
+
+    //todo: method1- Accessing Observable
+    /*const text$ = new Observable(function(subscriber) {
+      subscriber.next(1);
+      console.log('teasdst')
+    })*/
+
+    //todo: method2- Accessing Observable
+    /*const text$ = new Observable(function Subscribe(subscriber:Subscriber<any>) {
+      subscriber.next(1);
+      console.log('teasdst')
+    })*/
+
+    //todo: method3- Accessing Observable
+    //producer method can send and receive data.
+    //By default producer method can support both sync and async call.
+
+    const text$ = new Observable(subscriber => {
+      console.log('test')
+      subscriber.next(1);
+      subscriber.next(2);
+      subscriber.next(3);
+
+      //todo: subscriber.error('manual subscriber error') - to generate manual error
+      // subscriber.error('manual subscriber error')
+
+      setTimeout(() => {
+        subscriber.next(4);
+        subscriber.complete()
+      }, 1000);
+
+      //  subscriber.complete() - bruteforce method to complete an event
+      subscriber.complete()
+    })
+
+    text$.subscribe(x => {
+        console.log(x)
+        console.log('from inside of the subscribe method')
+      }
+    );
+
+
+    //another way of executing observable from consumer method
+    //1) consumer method can be called as many as possible.
+    text$.subscribe({
+      next(x) {
+        console.log('\n\n another way x=' + x)
+      },
+      error(err) {
+        console.log('something wrong = '+err)
+      },
+      complete() {
+        console.log('done');
+      }
+    })
+  }
 }
+
+
+/*    SINGLE	  MULTIPLE
+Pull	Function	Iterator
+Push	Promise	  Observable*/
